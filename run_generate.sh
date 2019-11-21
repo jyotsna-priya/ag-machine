f7c0be6ef6e9f86d59b7baaa86799fca2f7aa286
jyotsna-priya <54497691+jyotsna-priya@users.noreply.github.com>
2019-11-21 12:48:15 -0800

    Delete run_generate.sh

--- run_generate.sh
+++
@@ -1,4 +0,0 @@
-crontab -l | grep -v 'generate_data.py' > cronfile
-echo "* * * * * /usr/local/bin/python3 /Users/avdas/robot_ws/NCP-porting/sit_tools/generate_data.py --sensor_list $1 --location $2 --edge_station_id $3" >> cronfile
-crontab -l 2>/dev/null; cat cronfile | crontab -
-rm cronfile
