2313aa9d0e8f9454ca4d5c9acf2e14bb2ea6e0bb
jyotsna-priya <54497691+jyotsna-priya@users.noreply.github.com>
2019-12-01 15:12:34 -0800

    Update restImplementation.py

--- backend/restImplementation.py
+++ backend/restImplementation.py
@@ -9,29 +9,23 @@ app = Flask(__name__)
 @app.route('/get_sensor_data', methods=['GET'])
 def get_sensor_data():
     # Code to get data from shadow
-
+    
     # Get endpoint and thingname from GET param
-    endpoint = request.args.get("endpoint")
+    endpoint = "a3bikkrdsdyhco-ats.iot.us-east-1.amazonaws.com"
     thingname = request.args.get("thingname")
     subprocess.check_call(["python3", "getShadowData.py", "-e", "{}".format(endpoint), "-n", "{}".format(thingname)])
     f = open("output.txt", "r")
-    data = json.loads(f.read())["state"]["desired"]["property"]
+    data = json.loads(f.read())["state"]["desired"]["property"]["sensor_data"]
     f.close()
     return data
 
-
 @app.route('/configure_edgestation', methods=['PUT'])
 def update_sensor():
-    f = open("input.txt", "r")
-    data = json.loads(f.read())
-    f.close()
-    message = ""
-    for sensor in data.keys():
-        message += "{}:{},".format(sensor, data[sensor])
-    message = message.strip(",")
-    print(message)
+    data = request.get_json()
+    endpoint = "a3bikkrdsdyhco-ats.iot.us-east-1.amazonaws.com"
     #Code to send sensor list to edge station through API
-    subprocess.check_call(["python3", "sendSensorList.py", "-e", "a3bikkrdsdyhco-ats.iot.us-east-1.amazonaws.com", "-r", "AmazonRootCA1.pem", "-w", "-id", "backend_vm", "-t", "sensorListTopic", "-M", "{}".format(message)])
+    subprocess.check_call(["python3", "sendSensorList.py", "-e", "{}".format(endpoint), "-n", "{}".format(data["thingname"]), "-s", "{}".format(data)])
+    return data
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',
