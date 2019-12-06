aef477bb7350263be16c700d0c2ea624ddf2189b
jyotsna-priya <54497691+jyotsna-priya@users.noreply.github.com>
2019-12-06 13:45:48 -0800

    Update view_data.php

--- backend/view_data.php
+++ backend/view_data.php
@@ -65,8 +65,6 @@ session_start();
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home</a></li>
         <li><a href="solutions.php">Solutions</a></li>
-        <li><a href="#">Pricing</a></li>
-        <li><a href="#">Documentation</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
